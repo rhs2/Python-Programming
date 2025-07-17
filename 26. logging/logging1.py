80c7ae7b6d4e72432d34c69667e3d9bf7947187b
Sium <rhs0@yahoo.com>
2025-07-18 03:30:19 +0600

    logging in advance

--- 26. logging/logging1.py
+++ 26. logging/logging1.py
@@ -1,6 +1,17 @@
 #using DEBUG
 import logging
-logging.basicConfig(filename ='test1.log',level =logging.DEBUG, format = '%(asctime)s:%(levelname)s:%(message)s')
+logger = logging.getLogger(__name__) #setting up logger for different root.
+file_handler = logging.FileHandler('test1.log')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(file_handler)
+formatter = logging.Formatter('%(levelname)s:%(message)s')
+file_handler.setFormatter(formatter)
+#1. creating logger __name__
+#2. creating file_handler
+#3. adding file handler to logger
+#4. creating formatter
+#5. adding that formatter to file handler
+#6. creating logger level
 
 def add(x,y): #adding func
     return x+y
@@ -13,16 +24,16 @@ def division(x,y): #division func
     return x/y
     
 num1 = 10
-num2 = 5
+num2 = 7
 
 add_result = add(num1,num2)
-logging.debug(f"add: {num1} + {num2}: {add_result}")
+logger.debug(f"add: {num1} + {num2}: {add_result}")
 
 sub_result = subtract(num1,num2)
-logging.debug(f"subtract: {num1} - {num2}: {sub_result}")
+logger.debug(f"subtract: {num1} - {num2}: {sub_result}")
 
 mul_result = multiply(num1,num2)
-logging.debug(f"multiply: {num1} * {num2}: {mul_result}")
+logger.debug(f"multiply: {num1} * {num2}: {mul_result}")
 
 div_result = division(num1,num2)
-logging.debug(f"divide: {num1} / {num2}: {div_result}")
+logger.debug(f"divide: {num1} / {num2}: {div_result}")
